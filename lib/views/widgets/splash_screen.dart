2ade5888c284fe90080bd97a29123f4d21f9377f
haidara24 <haidara.yousef993@gmail.com>
2025-01-28 15:18:33 +0300

    add systemuioverlaystyle to splash screen

--- lib/views/widgets/splash_screen.dart
+++ lib/views/widgets/splash_screen.dart
@@ -1,5 +1,7 @@
+import 'package:camion/helpers/color_constants.dart';
 import 'package:camion/views/screens/control_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -24,16 +26,37 @@ class _SplashScreenState extends State<SplashScreen> {
   }
 
   @override
+  void dispose() {
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(
+        statusBarIconBrightness: Brightness.dark, // Reset to default
+        statusBarColor: AppColor.deepBlack,
+        systemNavigationBarColor: AppColor.deepBlack,
+      ),
+    );
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        alignment: Alignment.center,
-        children: [
-          SvgPicture.asset(
-            "assets/images/splash.svg",
-            fit: BoxFit.fill,
-          ),
-        ],
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: const SystemUiOverlayStyle(
+        statusBarColor: Colors.white, // Make status bar transparent
+        statusBarIconBrightness:
+            Brightness.light, // Light icons for dark backgrounds
+        systemNavigationBarColor: Colors.white, // Works on Android
+        systemNavigationBarIconBrightness: Brightness.light,
+      ),
+      child: Scaffold(
+        body: Stack(
+          alignment: Alignment.center,
+          children: [
+            SvgPicture.asset(
+              "assets/images/splash.svg",
+              fit: BoxFit.fill,
+            ),
+          ],
+        ),
       ),
     );
   }
